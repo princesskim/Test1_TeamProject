6373f2308377c3229007a99d919285307b0f7b65
SIRIDESKTOP\Siri Kim <ksrksr777@gmail.com>
2026-03-18 22:20:14 +0900

    Other : MyActor 클래스 리팩토링
    충돌 연습을 위해 고의적으로 수정

--- Source/Test1_TeamProject/MyActor.cpp
+++ Source/Test1_TeamProject/MyActor.cpp
@@ -11,16 +11,13 @@ AMyActor::AMyActor()
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("AMyyActor has begun play!"));
+	//UE_LOG(LogTemp, Warning, TEXT("AMyyActor has begun play!"));
 	
 }
 
 void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	UE_LOG(LogTemp, Warning, TEXT("AMyyActor is ticking"));
+	//UE_LOG(LogTemp, Warning, TEXT("AMyyActor is ticking"));
 
 }
-
-
-//팀원들의 변경사항과 상관없이 난 나의 분량을 했다.
