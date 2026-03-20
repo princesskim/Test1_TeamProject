889792f069f4f1abea4910fcf855f7ea638116c6
SIRIDESKTOP\Siri Kim <ksrksr777@gmail.com>
2026-03-20 11:27:41 +0900

    언리얼 팀 프로젝트 생성

---
+++ Source/TeamProject_ex/TeamProject_ex.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TeamProject_ex.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TeamProject_ex, "TeamProject_ex" );
